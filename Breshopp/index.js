3e06e60570f411ad8a7c8306d58f82a43c1385f5
Robson C. da Silva <83130807+robcs9@users.noreply.github.com>
2023-05-30 14:35:42 -0300

    Atualizando package e inicializando o projeto em NodeJs

---
+++ Breshopp/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const 
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Testing');
+});
+app.post('/cadastrar', urlencoded )
+app.listen(port, () => {
+    console.log(`app is listening at ${port}`);
+});
+
+// representação das entidades em forma json e popular com 10 itens de cada
+
+let administradores = [
+
+];
+
+let categorias = [
+
+];
+
+let usuarios = [
+
+];
+
+let publicacoes = [
+
+];
+
+
+/*let usuarios = [
+    {
+        publicacoes : []
+    },
+    {
+
+    }
+];
+let publicacoes = [
+    {
+        publicacoes : []
+    }
+];
+let fotos */
